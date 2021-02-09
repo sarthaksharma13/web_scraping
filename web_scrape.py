b245f89d42b61e67e54aaea731cc5a31ca1b38f7
Sarthak Sharma <sarthak.alexrider@gmail.com>
2021-02-09 05:32:46 +0530

    handle URLs from file, and update in Chrome

--- web_scrape.py
+++ web_scrape.py
@@ -1,8 +1,8 @@
-from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 import json
 import os
-import urllib2
 import argparse
 import time
 import io
@@ -74,11 +74,13 @@ def fetch_image_urls(state_query):
 def persist_image(state_link,query,f):
   
 
-    print(state_link["url"])
+    #print(state_link["url"])
     image_content = requests.get(state_link["url"],stream=True).content
     image_file = io.BytesIO(image_content)
     try:
         image = Image.open(image_file).convert('RGB')
+        #print(state_link["file_path"])
+        #print(state_link["file_path"].split('/')[-1][:-4] + " " + state_link["url"] + "\n")
         image.save(state_link["file_path"])
         f.write(state_link["file_path"].split('/')[-1][:-4] + " " + state_link["url"] + "\n")
         return f
@@ -87,34 +89,76 @@ def persist_image(state_link,query,f):
         
 
 
+def save_from_query():
+    '''
+    Documentation: Saves images from web based on certain query words, defined in the variable 'queries' below
 
-# NEED TO DOWNLOAD CHROMEDRIVER, insert path to chromedriver inside parentheses in following line
-wd = webdriver.Chrome('/usr/local/bin/chromedriver')
-save_dir = './images/'
-queries = ["dented cars", "scratched cars"]  #change your set of querries here
-max_img_per_query=1500
-state_query=dict()
-state_link=dict()
-for query in queries:
-    if not os.path.exists(os.path.join(save_dir,query.replace(" ","-"))):
-        os.makedirs(os.path.join(save_dir,query.replace(" ","-")))
+    '''
+    # NEED TO DOWNLOAD CHROMEDRIVER, insert path to chromedriver inside parentheses in following line
+    #wd = webdriver.Chrome('/usr/local/bin/chromedriver')
+    wd = webdriver.Chrome(ChromeDriverManager().install())
+    save_dir = './from_script/'
+    queries = ["heavy damaged cars"]  #change your set of querries here
+    max_img_per_query=1500
+    state_query=dict()
+    state_link=dict()
+    for query in queries:
+        if not os.path.exists(os.path.join(save_dir,query.replace(" ","-"))):
+            os.makedirs(os.path.join(save_dir,query.replace(" ","-")))
 
-    f=open(os.path.join(save_dir,query.replace(" ","-")+'.txt'),'w')
-    wd.get('https://google.com')
-    search_box = wd.find_element_by_css_selector('input.gLFyf')
-    search_box.send_keys(query)
-    state_query["query"]=query
-    state_query["max_links_to_fetch"]=max_img_per_query
-    state_query["webdriver"]=wd
-    state_query["sleep_between_interactions"]=0.1
-    links = fetch_image_urls(state_query)
-    
-    for i,link in enumerate(links):
-       
-        state_link["url"]=link
-        state_link["file_path"]=os.path.join(save_dir,query.replace(" ","-"),str(i).zfill(5) + '.jpg')
-        f = persist_image(state_link,query,f)
-    
-    f.close()
-wd.quit()
-print("Done")
+        f=open(os.path.join(save_dir,query.replace(" ","_")+'.txt'),'w')
+        wd.get('https://google.com')
+        search_box = wd.find_element_by_css_selector('input.gLFyf')
+        search_box.send_keys(query)
+        state_query["query"]=query
+        state_query["max_links_to_fetch"]=max_img_per_query
+        state_query["webdriver"]=wd
+        state_query["sleep_between_interactions"]=0.1
+        links = fetch_image_urls(state_query)
+        
+        for i,link in enumerate(links):
+        
+            state_link["url"]=link
+            state_link["file_path"]=os.path.join(save_dir,query.replace(" ","-"),str(i).zfill(5) + '.jpg')
+            f = persist_image(state_link,query,f)
+        
+        f.close()
+        print("Done", query)
+    wd.quit()
+    print("Done")
+
+def save_from_url():
+
+    """
+    Documentation: Save images from a list of url. Generally loaded from a txt file.
+    """
+    # file_path = ['./heavy_damage.txt','./dented_cars.txt','./scratched_cars.txt']
+    # save_path = ['heavy_damage','dented_cars','scratched_cars']
+    file_path = ['./scratched_cars.txt']
+    save_path = ['scratched_cars']
+    for f_,s in zip(file_path, save_path):
+        ind=0
+        print("==> ",s)
+        with open(f_) as f:
+            lineList = f.readlines()
+        urls=[]
+        for l in lineList:
+            urls.append(l[:-2])
+        for url in urls:
+            print(url)
+            image_content = requests.get(url,stream=True).content
+            image_file = io.BytesIO(image_content)
+
+            try:
+                image = Image.open(image_file).convert('RGB')
+                image.save('./' + s + '/' + str(ind).zfill(4) + '.jpg')
+                ind+=1
+            except:
+                print("==> Unable to get, ",url)
+            
+        
+
+        f.close()
+        print("==> Done, ", s)
+save_from_query()
+#save_from_url()
